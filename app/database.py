4bf676312fe035ae95929697e9b746d0de10276f
sampletech18 <workproject947@gmail.com>
2025-07-25 20:29:16 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 db = SQLAlchemy()
+
+def init_app(app: Flask):
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://duct_db_user:SXQ9iAKpluAXibt4xcxhakJk4uoQCFko@dpg-d2075pp5pdvs73c6q740-a.singapore-postgres.render.com/duct_db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
