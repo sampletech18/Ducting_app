00abfce0809444600d1102176b0d4a20c98536ef
sampletech18 <workproject947@gmail.com>
2025-07-26 11:25:59 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from app.database import db
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,43 +14,36 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+
 class Vendor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
-    gst_number = db.Column(db.String(100), nullable=True)
-    address = db.Column(db.String(250), nullable=True)
+    gst_number = db.Column(db.String(50))
+    address = db.Column(db.String(250))
     contact_person = db.Column(db.String(100))
-    contact_email = db.Column(db.String(120))
+    contact_email = db.Column(db.String(100))
     contact_phone = db.Column(db.String(20))
     bank_name = db.Column(db.String(100))
     bank_account = db.Column(db.String(100))
     ifsc = db.Column(db.String(20))
-    additional_contacts = db.relationship('VendorContact', backref='vendor', cascade='all, delete-orphan')
 
-class VendorContact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'))
-    person_name = db.Column(db.String(100))
-    email = db.Column(db.String(120))
-    phone = db.Column(db.String(20))
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    enquiry_id = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(150), nullable=False)
-    location = db.Column(db.String(200))
+    enquiry_id = db.Column(db.String(100), unique=True)
+    name = db.Column(db.String(150))
+    location = db.Column(db.String(150))
     start_date = db.Column(db.Date)
     end_date = db.Column(db.Date)
-    source_drawing = db.Column(db.String(300))
     vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'))
-    quotation = db.Column(db.String(100))
-    project_incharge = db.Column(db.String(100))
-    email = db.Column(db.String(120))
-    phone = db.Column(db.String(20))
-    gst_number = db.Column(db.String(100))
+    gst_number = db.Column(db.String(50))
     address = db.Column(db.String(250))
-    vendor = db.relationship('Vendor', backref='projects')
-    measurement_sheets = db.relationship('MeasurementSheet', backref='project', cascade='all, delete-orphan')
+    quotation = db.Column(db.String(250))
+    project_incharge = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
+    source_drawing = db.Column(db.String(250))
+
 
 class MeasurementSheet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -71,11 +65,3 @@ class MeasurementSheet(db.Model):
     cleat = db.Column(db.Float)
     corner = db.Column(db.Integer)
     nuts = db.Column(db.Integer)
-
-class Employee(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    role = db.Column(db.String(100))
-    contact = db.Column(db.String(20))
-    email = db.Column(db.String(120))
-    address = db.Column(db.String(200))
