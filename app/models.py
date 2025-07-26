35ab3d4f495c2667f27b153642a759f77cde5743
sampletech18 <workproject947@gmail.com>
2025-07-26 11:20:51 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,17 @@
 from app.database import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 class Vendor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
