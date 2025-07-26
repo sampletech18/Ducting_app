f8f11267ea3c9498bc619418a8b82eb5468da99b
sampletech18 <workproject947@gmail.com>
2025-07-26 11:12:53 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
 from flask import Flask
-from app.database import db
-from app.routes import main
+from .database import db
+from .routes import main as main_blueprint
+from .models import User
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'your_secret_key'
-
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://duct_db_user:SXQ9iAKpluAXibt4xcxhakJk4uoQCFko@dpg-d2075pp5pdvs73c6q740-a.singapore-postgres.render.com/duct_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    app.register_blueprint(main)
+    app.register_blueprint(main_blueprint)
+
+    with app.app_context():
+        db.create_all()
+
+        # Dummy user: admin / admin123
+        if not User.query.filter_by(username='admin').first():
+            admin = User(username='admin')
+            admin.set_password('admin123')
+            db.session.add(admin)
+            db.session.commit()
 
     return app
